024c25e65606656e7d6f913b5a8a9bd958850d09
Jaehyun Lee <jaehylee@c1r24s1.42gyeongsan.kr>
2024-12-23 03:00:59 +0900

    init header

---
+++ src/fdf.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fdf.h                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jaehylee <jaehylee@student.42gyeongsan.kr> +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/22 22:29:49 by jaehylee          #+#    #+#             */
+/*   Updated: 2024/12/23 00:03:43 by jaehylee         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FDF_H
+# define FDF_H
+# include <fcntl.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <string.h>
+# include "../ft_printf/src/ft_printf.h"
+# include "../minilibx-linux/mlx.h"
+#endif
